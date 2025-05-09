d4728c14e4e137b10906a5dd04cdec1cd89c7e01
Alexey Sokolov Top <sokolikas076@gmail.com>
2025-05-09 16:11:07 +0300

    Страница скроллится более удобно

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:code_generator_app/data/inherited/directory_functions_inherited.dart';
+import 'package:code_generator_app/data/inherited/text_fields_functions_inherited.dart';
 import 'package:code_generator_app/ui/main/main_wm.dart';
 import 'package:code_generator_app/ui/main/widgets/directory_widget/directory_widget.dart';
 import 'package:code_generator_app/ui/main/widgets/enter_button.dart';
@@ -33,61 +34,65 @@ class MainScreen extends ElementaryWidget<IMainScreenWidgetModel> {
             ),
             Center(
               child: SingleChildScrollView(
+                reverse: true,
                 //TODO прокидывать методы через inherited
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Logo(),
-                    const TextLargeTitleWidget(),
-                    SizedBox(
-                      height: MediaQuery.of(wm.context).size.height / 10,
-                    ),
-                    LoginTextField(
-                      isLoginObscuredListenable: wm.isLoginObscuredListenable,
-                      loginController: wm.loginController,
-                      focusNode: wm.loginFocusNode,
-                      onObscureLoginTap: () => wm.onObscureLoginTap(),
-                      onTapOutside: () => wm.onTapOutsideField(),
-                      onFieldSubmitted: () => wm.onNextTapFronLogin(),
-                    ),
-                    const SizedBox(height: 15),
-                    TextFormField(
-                      decoration: const ThemedInputDecoration(
-                        labelText: 'Название сайта',
-                        prefixIcon: Icon(Icons.web_asset),
+                child: FieldsFuncs(
+                  onTapOutsideField: () => wm.onTapOutsideField(),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Logo(),
+                      const TextLargeTitleWidget(),
+                      SizedBox(
+                        height: MediaQuery.of(wm.context).size.height / 10,
                       ),
-                      controller: wm.wordController,
-                      focusNode: wm.websiteFocusNode,
-                      textInputAction: TextInputAction.next,
-                      onTapOutside: (_) => wm.onTapOutsideField(),
-                    ),
-                    const SizedBox(height: 15),
-                    KeyTextField(
-                      isKeyObscuredListenable: wm.isKeyObscuredListenable,
-                      keyController: wm.keyController,
-                      focusNode: wm.keywordFocusNode,
-                      onObscureKeyTap: () => wm.onObscureKeyTap(),
-                      onTapOutside: () => wm.onTapOutsideField(),
-                      onFieldSubmitted: () => wm.onEnterTap(),
-                    ),
-                    const SizedBox(height: 5),
-                    MiddleBarWidget(
-                      doSaveListenable: wm.doSaveListenable,
-                      onSaveCheckTap: () => wm.onSaveCheckTap(),
-                      onGuideTap: () => wm.onGuideTap(),
-                    ),
-                    const SizedBox(height: 5),
-                    PasswordField(
-                      result: wm.result,
-                      onTap: () => wm.onPasswordTap(),
-                    ),
-                    const SizedBox(height: 15),
-                    EnterButton(
-                      listenableEntityState: wm.encryptionAlgorithmListenable,
-                      onEnterTap: () => wm.onEnterTap(),
-                    ),
-                    const SizedBox(height: 25),
-                  ],
+                      LoginTextField(
+                        isLoginObscuredListenable: wm.isLoginObscuredListenable,
+                        loginController: wm.loginController,
+                        focusNode: wm.loginFocusNode,
+                        onObscureLoginTap: () => wm.onObscureLoginTap(),
+                        onTapOutside: () => wm.onTapOutsideField(),
+                        onFieldSubmitted: () => wm.onNextTapFronLogin(),
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        decoration: const ThemedInputDecoration(
+                          labelText: 'Название сайта',
+                          prefixIcon: Icon(Icons.web_asset),
+                        ),
+                        controller: wm.wordController,
+                        focusNode: wm.websiteFocusNode,
+                        textInputAction: TextInputAction.next,
+                        onTapOutside: (_) => wm.onTapOutsideField(),
+                      ),
+                      const SizedBox(height: 15),
+                      KeyTextField(
+                        isKeyObscuredListenable: wm.isKeyObscuredListenable,
+                        keyController: wm.keyController,
+                        focusNode: wm.keywordFocusNode,
+                        onObscureKeyTap: () => wm.onObscureKeyTap(),
+                        onTapOutside: () => wm.onTapOutsideField(),
+                        onFieldSubmitted: () => wm.onEnterTap(),
+                      ),
+                      const SizedBox(height: 5),
+                      MiddleBarWidget(
+                        doSaveListenable: wm.doSaveListenable,
+                        onSaveCheckTap: () => wm.onSaveCheckTap(),
+                        onGuideTap: () => wm.onGuideTap(),
+                      ),
+                      const SizedBox(height: 5),
+                      PasswordField(
+                        result: wm.result,
+                        onTap: () => wm.onPasswordTap(),
+                      ),
+                      const SizedBox(height: 15),
+                      EnterButton(
+                        listenableEntityState: wm.encryptionAlgorithmListenable,
+                        onEnterTap: () => wm.onEnterTap(),
+                      ),
+                      const SizedBox(height: 25),
+                    ],
+                  ),
                 ),
               ),
             ),
