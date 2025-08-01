431618fb055a1fd73332fde84a7f31f62f6e68b2
Alexey Sokolov Top <sokolikas076@gmail.com>
2025-08-01 13:35:12 +0300

    еще метаданные

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"code_generator_app", origin, size)) {
+  Win32Window::Size size(400, 800);
+  if (!window.Create(L"PasSculptor", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
